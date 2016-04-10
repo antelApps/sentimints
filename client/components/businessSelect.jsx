06d712bf872ae4b2a17368b05876beb4ca23f6fa
austinjkovach <kovach.a.j@gmail.com>
2016-04-10 00:37:39 -0500

    basic styling started

--- client/components/businessSelect.jsx
+++ client/components/businessSelect.jsx
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom"
 
 export default class BusinessSelect extends React.Component{
 
-	handleChange(e){
-		this.props.onChange(e.target.value);
-	}
+  handleChange(e){
+    this.props.onChange(e.target.value);
+  }
 
-	render(){
-		return (
-			<div>
-			  <select onChange={this.handleChange.bind(this)}>
-			  	{this.props.businessNames.map(function(cur, index){
-			  		return (<option key={index} value={cur}> {cur} </option>)
-			  	})}
-			  </select>
-			</div>	
-		)
-	}
+  render(){
+    return (
+      <div className="selectContainer businessSelect">
+        <select className="form-control" onChange={this.handleChange.bind(this)}>
+          {this.props.businessNames.map(function(cur, index){
+            console.log('inside', cur, index)
+            return (<option key={index} value={cur}> {cur} </option>)
+          })}
+        </select>
+      </div>  
+    )
+  }
 }
--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -7,65 +7,72 @@ import DatePicker from 'react-date-picker'
 require('react-date-picker/index.css'); //once css loader works should be fine.
 
 
-var businessNames = ["test", "test2", "test3"];
+var businessNames = ["-- Select a Business --", "test", "test2", "test3"];
 
 export default class Menu extends React.Component{
 
-	constructor() {
-		super();
+  constructor() {
+    super();
     this.state = {
-    	startDate: null,
-    	endDate: null,
-    	business: businessNames[0],
-    	star: 1
+      startDate: null,
+      endDate: null,
+      business: businessNames[0],
+      star: 1
     };
   }
 
-	handleDateChange(dateString, moment){
-		console.log('stuff changed', arguments);
-	}
+  handleDateChange(dateString, moment){
+    console.log('stuff changed', arguments);
+  }
 
-	handleBusinessChange(newState){
-		var component = this;
-		this.setState({business: newState}, function(){
-			console.log("new business value: ", component.state.business, newState);		
-		})
-	}
+  handleBusinessChange(newState){
+    var component = this;
+    this.setState({business: newState}, function(){
+      console.log("new business value: ", component.state.business, newState);    
+    })
+  }
 
-	handleStarChange(newState){
-		var component = this;
-		this.setState({star: newState}, function(){
-			console.log("new star value: ", component.state.star, newState);		
-		})
-	}
+  handleStarChange(newState){
+    var component = this;
+    this.setState({star: newState}, function(){
+      console.log("new star value: ", component.state.star, newState);    
+    })
+  }
 
-	handleButtonClick(){
-		this.props.onSearch(this.state.startDate, this.state.endDate, this.state.business, this.state.star);
-	}
+  handleButtonClick(){
+    this.props.onSearch(this.state.startDate, this.state.endDate, this.state.business, this.state.star);
+  }
 
-	render(){
-		return (
-			<div>
-				Start Date:
-				<DatePicker
-				  minDate='2006-04-04'
-				  maxDate='2006-10-10'
-				  date={Date.now()}
-				  onChange={this.handleDateChange.bind(this, "start")}
-				/>
-				<DatePicker
-				  minDate='2014-04-04'
-				  maxDate='2015-10-10'
-				  date={Date.now()}
-				  onChange={this.handleDateChange.bind(this, "end")}
-				/>
+  render(){
+    return (
+      <div className="container col-md-offset-2 col-md-8 col-sm-offset-0 col-sm-12">
 
-				<BusinessSelect businessNames={businessNames} onChange={this.handleBusinessChange.bind(this)}/>
-
-				<StarSelect onChange={this.handleStarChange.bind(this)}/>
-
-				<button onClick={this.handleButtonClick.bind(this)}> Go </button>
-			</div>
-		)
-	}
+        <div className="date-container left">
+          Start Date:
+          <DatePicker
+            minDate={'2006-04-04'}
+            maxDate={'2006-10-10'}
+            date={Date.now()}
+            onChange={this.handleDateChange.bind(this, "start")}
+          />
+        </div>
+        <div className="date-container right">
+          End Date:
+          <DatePicker
+            minDate='2014-04-04'
+            maxDate='2015-10-10'
+            date={Date.now()}
+            onChange={this.handleDateChange.bind(this, "end")}
+          />
+        </div>
+        <br/>
+        <br/>
+        <div className="selectors">
+          <BusinessSelect businessNames={businessNames} onChange={this.handleBusinessChange.bind(this)}/>
+          <StarSelect onChange={this.handleStarChange.bind(this)}/>
+          <button className="btn btn-md btn-primary col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-0" onClick={this.handleButtonClick.bind(this)}> Go </button>
+        </div>
+      </div>
+    )
+  }
 }
