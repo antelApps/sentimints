61886ec2c7461c2ee39316240e4d6db007d8e0fc
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-04-10 12:43:20 -0500

    giving back in order we want

--- server/sentiment.js
+++ server/sentiment.js
@@ -374,12 +374,33 @@ let repackageAsArray = function(summStatObj, nameString, groupString) {
   Takes an array of topic objects
   Transforms each according to repackage as array, using the topic as the nameString
 */
-let repackageTopicArr = function(arr, groupString) {
+let repackageTopicArr = function(arr, groupString, sortFunc) {
 	return arr.map( (topicObj) => {
 		let topic = topicObj.topic;
 		let statsArray = repackageAsArray(topicObj.stats, topic, groupString);
 		console.log('statsArray for topic', topic, 'is', statsArray);
 		return {topic: topic, stats: statsArray}
+	}).sort( (a, b) => {
+		return sortFunc(a, b);
+	})
+}
+
+
+let lowestFirst = function(arr, groupString) {
+	return repackageTopicArr(arr, groupString, function(a, b) {
+		return a.stats.median < b.stats.median;
+	})
+}
+
+let highestFirst = function(arr, groupString) {
+	return repackageTopicArr(arr, groupString, function(a, b) {
+		return a.stats.median > b.stats.median;
+	})
+}
+
+let popularFirst = function(arr, groupString) {
+	return repackageTopicArr(arr, groupString, function(a, b) {
+		return a.stats.num > b.stats.num;
 	})
 }
 
@@ -430,9 +451,9 @@ let getAll = function(arrayOfTexts) {
 		let summarizedTopics = topicsScores(topicsMap(allSentiments));
 		let frequencySummaryStatistics = topicFrequencies(summarizedTopics);
 		let medianSummaryStatistics = summStatsOfMedians(summarizedTopics);
-		allData.topical.worst = repackageTopicArr( getBottomQuartile(summarizedTopics, medianSummaryStatistics), "worst");
-		allData.topical.best = repackageTopicArr( getTopQuartile(summarizedTopics, medianSummaryStatistics), "best");
-		allData.topical.hot = repackageTopicArr( mostPopular(summarizedTopics, frequencySummaryStatistics), "hot");
+		allData.topical.worst = lowestFirst( getBottomQuartile(summarizedTopics, medianSummaryStatistics), "worst");
+		allData.topical.best = highestFirst( getTopQuartile(summarizedTopics, medianSummaryStatistics), "best");
+		allData.topical.hot = popularFirst( mostPopular(summarizedTopics, frequencySummaryStatistics), "hot");
 
 		return allData;
 	})
@@ -456,24 +477,26 @@ module.exports.getAllFromReviews = getAllFromReviews;
 
 // The below is for 'tests'
 
-// let myWords = [
-//   'Amazing So nice people Helpful Friendly Wonderful food Amazing unique drinks and ingredients',
-//   'Went on a Friday night with a group of friends, service was good considering we had a fairly large group, the food was average.The best part of the meal was the nachos we had as an appetizer. The atmosphere was great, the bartender was very knowledgeable, overall was just hoping the food would have been better.',
-//   "This small California-based chain brings an upscale bar and dining experience to Dirty Sixth just across the street from The Driskill Hotel. I've only come here for drinks during SXSW, so my review does not pertain to the culinary experience. A large central bar occupies the majority of this establishment and offers up a large selection of craft beers and whisky. They have solid daily specials and two happy hours each day (2-6PM and 9-11PM).",
-//   "We stumbled into this little restaurant on a quest for onion rings but decided to stay for a full meal. The interior is industrial and beautiful and the restaurant had an inviting, friendly vibe.",
-//   "We ordered the panko onion rings to share, which ended up being three of the biggest onion rings I've ever seen in my life. They were tasty but nothing to write home about. I was a bit disappointed by the slow roasted beet salad. I had been super excited about the watermelon pop rocks that they include in their version of this classic salad, but the experience was lackluster. The residual moisture in the salad made the rocks pop too early, so I was left with pink goo sprinkled around my plate rather than the nice crackling experience I was anticipating. The beets were also not great. They were undercooked and chopped into enormous chunks that I found unmanageable.",
-//   "We started with one waitress who was really wonderful, but we must have caught her at the end of her shift because she disappeared halfway through. Still, service was friendly and efficient.",
-//   "It was happy hour, so I asked my bartender which food special he recommended. Together we went with the chicken tacos. They did not disappoint. (Note: I didn't have a single disappointing taco in this town, including the airport). My bartender also gave me many samples of beers to help me make my selection. (Well, he did that until he got too busy to give me as much special attention--bartender attention is just one of the many benefits to day drinking). ",
-//   "In short, Yelp wins again! Thanks to the Yelpers who visited before me to help me drink good beer and nosh good tacos.",
-//   "I've been to multiple Eurekas around the country and I must say this is probably the best one I've been to. Great beer selection with many local taps. Love the atmosphere and setup. Great location as well. "
-// ]
+let myWords = [
+  'Amazing So nice people Helpful Friendly Wonderful food Amazing unique drinks and ingredients',
+  'Went on a Friday night with a group of friends, service was good considering we had a fairly large group, the food was average.The best part of the meal was the nachos we had as an appetizer. The atmosphere was great, the bartender was very knowledgeable, overall was just hoping the food would have been better.',
+  "This small California-based chain brings an upscale bar and dining experience to Dirty Sixth just across the street from The Driskill Hotel. I've only come here for drinks during SXSW, so my review does not pertain to the culinary experience. A large central bar occupies the majority of this establishment and offers up a large selection of craft beers and whisky. They have solid daily specials and two happy hours each day (2-6PM and 9-11PM).",
+  "We stumbled into this little restaurant on a quest for onion rings but decided to stay for a full meal. The interior is industrial and beautiful and the restaurant had an inviting, friendly vibe.",
+  "We ordered the panko onion rings to share, which ended up being three of the biggest onion rings I've ever seen in my life. They were tasty but nothing to write home about. I was a bit disappointed by the slow roasted beet salad. I had been super excited about the watermelon pop rocks that they include in their version of this classic salad, but the experience was lackluster. The residual moisture in the salad made the rocks pop too early, so I was left with pink goo sprinkled around my plate rather than the nice crackling experience I was anticipating. The beets were also not great. They were undercooked and chopped into enormous chunks that I found unmanageable.",
+  "We started with one waitress who was really wonderful, but we must have caught her at the end of her shift because she disappeared halfway through. Still, service was friendly and efficient.",
+  "It was happy hour, so I asked my bartender which food special he recommended. Together we went with the chicken tacos. They did not disappoint. (Note: I didn't have a single disappointing taco in this town, including the airport). My bartender also gave me many samples of beers to help me make my selection. (Well, he did that until he got too busy to give me as much special attention--bartender attention is just one of the many benefits to day drinking). ",
+  "In short, Yelp wins again! Thanks to the Yelpers who visited before me to help me drink good beer and nosh good tacos.",
+  "I've been to multiple Eurekas around the country and I must say this is probably the best one I've been to. Great beer selection with many local taps. Love the atmosphere and setup. Great location as well. "
+]
 
-// return getAll(myWords)
-//   .then(function(x) {
-//   	console.log('got x', x)
-//   	console.log('x has topic thing', x.topical);
-//   	console.log('best', x.topical.best);
-//   })
+return getAll(myWords)
+  .then(function(x) {
+  	console.log('got x', x)
+  	console.log('x has topic thing', x.topical);
+  	console.log('best', x.topical.best, 'first best', x.topical.best[0].stats, 'second best', x.topical.best[1].stats);
+  	console.log('worst', x.topical.worst, 'first worst', x.topical.worst[0].stats, 'second worst', x.topical.worst[1].stats);
+  	console.log('hot', x.topical.hot, 'first hottest', x.topical.hot[0].stats, 'second hottest', x.topical.hot[1].stats);
+  })
 
 
 
