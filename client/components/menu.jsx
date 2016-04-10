91244186af563f7c508ffa83e89a6a29f95e00a7
frankievx <frankievx@gmail.com>
2016-04-09 19:43:49 -0500

    include star select

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import DatePicker from 'react-date-picker'
 import BusinessSelect from './businessSelect.jsx'
+import StarSelect from './starSelect.jsx'
 // import 'react-date-picker.css'
 // require('react-date-picker/index.css'); //once css loader works should be fine.
 
@@ -13,9 +14,10 @@ export default class Menu extends React.Component{
 	constructor() {
 		super();
     this.state = {
-    	selectValue: businessNames[0],
     	startDate: null,
-    	endDate: null
+    	endDate: null,
+    	business: businessNames[0],
+    	star: 1
     };
   }
 
@@ -23,15 +25,22 @@ export default class Menu extends React.Component{
 		console.log('stuff changed', arguments);
 	}
 
-	handleSelectChange(newState){
+	handleBusinessChange(newState){
 		var component = this;
-		this.setState({selectValue: newState}, function(){
-			console.log("new select value: ", component.state.selectValue, newState);		
+		this.setState({business: newState}, function(){
+			console.log("new business value: ", component.state.business, newState);		
+		})
+	}
+
+	handleStarChange(newState){
+		var component = this;
+		this.setState({star: newState}, function(){
+			console.log("new star value: ", component.state.star, newState);		
 		})
 	}
 
 	handleButtonClick(){
-		this.props.onSearch(this.state.startDate, this.state.endDate, this.state.selectValue);
+		this.props.onSearch(this.state.startDate, this.state.endDate, this.state.business, this.state.star);
 	}
 
 	render(){
@@ -52,7 +61,10 @@ export default class Menu extends React.Component{
 				  date={Date.now()}
 				  onChange={this.handleDateChange.bind(this, "end")}
 				/>
-				<BusinessSelect businessNames={businessNames} onChange={this.handleSelectChange.bind(this)}/>
+
+				<BusinessSelect businessNames={businessNames} onChange={this.handleBusinessChange.bind(this)}/>
+
+				<StarSelect onChange={this.handleStarChange.bind(this)}/>
 
 				<button onClick={this.handleButtonClick.bind(this)}> Go </button>
 			</div>
