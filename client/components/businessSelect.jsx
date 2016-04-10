58ba0cd3caab70b50b84473c48a768f549668560
austinjkovach <kovach.a.j@gmail.com>
2016-04-10 09:24:22 -0500

    Removed merge conflicts

--- client/components/businessSelect.jsx
+++ client/components/businessSelect.jsx
@@ -7,7 +7,6 @@ export default class BusinessSelect extends React.Component{
     this.props.onChange(e.target.value);
   }
 
-<<<<<<< cbd0624fe73cf244f0980d6fd1b837a49e2a0ae2
 	render(){
 		return (
 			<div>
@@ -20,18 +19,4 @@ export default class BusinessSelect extends React.Component{
 		)
 	}
 }
-=======
-  render(){
-    return (
-      <div className="selectContainer businessSelect">
-        <select className="form-control" onChange={this.handleChange.bind(this)}>
-          {this.props.businessNames.map(function(cur, index){
-            console.log('inside', cur, index)
-            return (<option key={index} value={cur}> {cur} </option>)
-          })}
-        </select>
-      </div>  
-    )
-  }
-}
->>>>>>> basic styling started
+
