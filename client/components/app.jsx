6034cec85e91562797850b74614d0417bb62e1d6
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-04-10 00:52:04 -0500

    handleSearch in app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,23 +1,80 @@
 import React from "react"
 import ReactDOM from "react-dom";
+import Promise from "bluebird";
 
 import Menu from "./menu.jsx"
 import Navbar from "./navbar.jsx"
 import AggChart from './aggChart.jsx'
-// import Results from "./results.jsx"
 import UserActions from '../actions/userActions.jsx'
 
 class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {selectValue: null};
+    this.state = {
+      selectValue: null,
+      data_aggregate: null,
+      data_topical: null,
+      data_label: 'All Reviews'
+    };
   }
 
   handleSearch(startDate, endDate, business, star){
     console.log("handleSearch args", arguments)
 
-    //ajax call to server
+    //Package the dates into the object the requests are expecting
+    let dateRange = startDate && endDate ? [startDate, endDate] : undefined;
+
+    //Format text for label
+    let getDateRangeText = function() {
+      return dateRange ? ' from ' + startDate + ' to ' + endDate : ''
+    }
+    let getStarText = function() {
+      if (star) {
+        return star[0] === star[1] ? ' with ' + star[0] + ' Stars' : ' with ' + star[0] + '-' + star[1] + ' Stars';
+      } else {
+        return '';
+      }
+    }
+    let getlabelText = function() {
+      let dateRangeText = getDateRangeText();
+      let starText = getStarText();
+      return (dateRangeText.length === 0 && starText.length === 0 ? 'All Reviews' : 'Reviews' + dateRangeText + starText)
+    }
+
+    //Make the request and update the state
+    return new Promise( function(resolve, reject) {
+      if (dateRange && star) {
+        return UserActions.getAllReviews(business, star, dateRange)
+          .then( function(data) {
+            resolve(data);
+          })
+      } else if (dateRange) {
+        return UserActions.getReviewsByDate(business, dateRange)
+          .then( function(data) {
+            resolve(data);
+          })
+      } else if (star) {
+        return UserActions.getReviewsByStars(business, star)
+          .then( function(data) {
+            resolve(data);
+          })
+      } else {
+        return UserActions.getAllReviews(business)
+        .then( function(data) {
+          resolve(data);
+        })
+      }
+    })
+    .then( function(data) {
+      this.setState(data_label, getlabelText());
+      this.setState(data_aggregate, data.aggregate);
+      this.setState(data_topical, data.data_topical);
+    })
+    .catch( function(err) {
+      console.log('Error handling search', err);
+    })
+
   }
 
 	render() {
