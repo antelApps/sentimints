a96d0dfe38a3a1880d48f77e785e6d7237e59bfe
Julia Brown <juliacelestebrown@gmail.com>
2016-04-10 14:43:13 -0500

    last little clean ups

--- client/actions/userActions.jsx
+++ client/actions/userActions.jsx
@@ -10,14 +10,12 @@ UserActions.getAllReviews = function(name, stars = [0, 5], dateRange = ['1900-01
     let uri = '/api/mint?business_name=' + URIname + '&business_stars=' + stars[0] + '_' + stars[1] + '&business_dates=' + dateRange[0] + '_' + dateRange[1];
     $.ajax({ url: uri })
       .done((data) => {
-        console.log('GOT DATA!', data);
         resolve(data);
       })
       .fail((err) => {
         if (err.status === 400) {
           alert(err.responseText);
         }
-        console.error("Didn't get data :(", err);
         reject(err);
       });      
     })
@@ -29,14 +27,12 @@ UserActions.getReviewsByStars = function(name, stars) {
     let uri = '/api/mint/stars?business_name=' + URIname + '&business_stars=' + stars[0] + '_' + stars[1];
     $.ajax({url: uri})
       .done((data) => {
-        console.log('GOT DATA!', data);
         resolve(data);
       })
       .fail((err) => {
         if (err.status === 400) {
           alert(err.responseText);
         }
-        console.error("Didn't get data :(", err);
         reject(err);
       });      
     })
@@ -48,14 +44,12 @@ UserActions.getReviewsByDate = function(name, dateRange) {
     let uri = '/api/mint/date?business_name=' + URIname + '&business_dates=' + dateRange[0] + '_' + dateRange[1];
     $.ajax({url: uri})
       .done((data) => {
-        console.log('GOT DATA!', data);
         resolve(data);
       })
       .fail((err) => {
         if (err.status === 400) {
           alert(err.responseText);
         }
-        console.error("Didn't get data :(", err);
         reject(err);
       });      
   })
